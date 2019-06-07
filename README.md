09554051a088ca4723dacf25a815756e3fd28325
Anthony Rhundell Lee <yeenot.ar@gmail.com>
2019-06-07 17:14:49 +0800

    upload app.vue (missing file)

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # pshs-passers
 A single web application for PSHS Passers Code Challenge using Laravel-Vue
-
+<br/>Copy .env.example to .env
 <br/>Run the folllowing commands: <br/><br/>
 1.) `composer install` <br/>
 2.) `npm install` <br/>
-3.) `php artisan migrate` <br/>
-4.) `php artisan db:seed` (the seed will grab the data from PSHS passers url) <br/>
-5.) `npm run watch` <br/>
-6.) `php artisan serve` <br/>
+3.) `php artisan key:generate` <br/>
+4.) `php artisan migrate` <br/>
+5.) `php artisan db:seed` (the seed will grab the data from PSHS passers url) <br/>
+6.) `npm run watch` <br/>
+7.) `php artisan serve` <br/>
---
+++ resources/views/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="menu">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item" v-for="(item, index) in menu" :key="index" :class="{ 'active': $route.name == item.name }">
+            <router-link class="nav-link" :to="{ name: item.name}" >{{ item.label }}</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    menu: [
+      {
+        name: 'home',
+        label: 'Home'
+      },
+      {
+        name: 'school',
+        label: 'School List'
+      }
+    ]
+  })
+};
+</script>
+
+<style lang="scss" scoped>
+  #menu {
+    font-size: 17px;
+    text-transform: uppercase;
+
+    .nav-item + .nav-item {
+      margin-left: 10px;
+    }
+  }
+</style>
